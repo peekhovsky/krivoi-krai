de913309f12477d7c74c9b4ba7a8171883ec948a
Rastsislau Piakhouski <rastsislau_piakhouski@epam.com>
2019-11-14 17:17:30 +0300

    add slick

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import _ from 'lodash';
+import slick from 'slick-carousel'
+import $ from 'jquery'
+
+$(document).ready(() => {
+    $('.gal').slick({
+        slidesToShow: 3,
+        dots:true,
+        centerMode: true,
+    })
+});
